c3963695270523d593749d1f0071f2643c7e8238
dravit <dravit.info@gmail.com>
2017-03-24 00:06:19 +0530

    Commit

--- app/src/main/java/com/example/bmc/LoginActivity.java
+++ app/src/main/java/com/example/bmc/LoginActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.support.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-
+import com.google.android.gms.auth.api.Auth;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -16,16 +16,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.bmc.R;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import butterknife.ButterKnife;
 import butterknife.Bind;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
-
+    private TextView mStatusTextView;
     @Bind(R.id.btn_login) Button _loginButton;
     @Bind(R.id.input_phone)EditText phoneNumber;
+    GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,22 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
+        findViewById(R.id.btn_login_with_gmail).setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.btn_login_with_gmail);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -50,6 +76,11 @@ public class LoginActivity extends AppCompatActivity {
         Intent mainActivityIntern = new Intent(this,MainActivity.class);
         startActivity(mainActivityIntern);
         overridePendingTransition(R.animator.activity_open_scale,R.animator.activity_close_translate);
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     public void login() {
@@ -82,6 +113,12 @@ public class LoginActivity extends AppCompatActivity {
         callMainActivity();
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -92,6 +129,24 @@ public class LoginActivity extends AppCompatActivity {
                 // By default we just finish the Activity and log them in automatically
                 this.finish();
             }
+        }
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            callMainActivity();
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
         }
     }
 
@@ -119,5 +174,14 @@ public class LoginActivity extends AppCompatActivity {
         }
 
         return valid;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login_with_gmail:
+                signIn();
+                break;
+        }
     }
 }
