d7f72728b8cb43b34d362457f3c8b3cef5a7db9e
arjun <arjuntirumalasetty@gmail.com>
2017-03-27 23:33:57 +0530

    Commiting google maps utility

--- app/src/main/java/com/example/bmc/MapsActivity.java
+++ app/src/main/java/com/example/bmc/MapsActivity.java
@@ -1,7 +1,15 @@
 package com.example.bmc;
 
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,7 +20,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final int MY_REQUEST_ACCESS_COARSE_LOCATION = 0;
     private GoogleMap mMap;
+    private String ENABLE_PIN;
+    private static final int MY_REQUEST_ACCESS_FINE_LOCATION =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,51 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if(getIntent().getStringExtra("ENABLE_PIN")!=null){
+            ENABLE_PIN = getIntent().getStringExtra("ENABLE_PIN");
+
+        }
+    }
+
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        Log.i("Longitude ",longitude+"");
+        Log.i("Latitude ",latitude+"");
+    }
+
+    public void getCurrentLocation(){
+        try{
+            if (mMap != null) {
+
+                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_REQUEST_ACCESS_FINE_LOCATION);
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                            MY_REQUEST_ACCESS_COARSE_LOCATION);
+                    Log.i("ACCESS_COARSE_LOCATION",MY_REQUEST_ACCESS_COARSE_LOCATION+"");
+                } else{
+                    mMap.setMyLocationEnabled(true);
+                    LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+                    Criteria criteria = new Criteria();
+                    String bestLocationProvider = locationManager.getBestProvider(criteria,true);
+                    Location location = locationManager.getLastKnownLocation(bestLocationProvider);
+                    if (location != null) {
+                        onLocationChanged(location);
+                    }
+                    locationManager.requestLocationUpdates(bestLocationProvider, 20000, 0, (LocationListener) this);
+                }
+            }
+        }catch(Exception e){
+            Log.e("Error in getLoccation",e.getMessage());
+        }
+
     }
 
 
@@ -36,11 +92,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
+        try {
+            mMap = googleMap;
+            getCurrentLocation();
+        }catch(Exception e){
+            Log.e("Error in onMapReady ",e.getMessage());
+        }
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
--- app/src/main/java/fragments/AddStadiumFragment.java
+++ app/src/main/java/fragments/AddStadiumFragment.java
@@ -1,22 +1,41 @@
 package fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.bmc.MapsActivity;
 import com.example.bmc.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Arjun on 3/23/2017.
  */
 
 public class AddStadiumFragment extends Fragment {
-
+    @Bind(R.id.btn_tap_to_pin)
+    Button pinMyLocation_Btn;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.add_stadium_fragment,container,false);
+        View view = inflater.inflate(R.layout.add_stadium_fragment,container,false);
+        ButterKnife.bind(this,view);
+        pinMyLocation_Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mapView = new Intent(getActivity(), MapsActivity.class);
+                mapView.putExtra("ENABLE_PIN","enable");
+                getActivity().startActivity(mapView);
+            }
+        });
+
+        return view;
     }
+    
 }
